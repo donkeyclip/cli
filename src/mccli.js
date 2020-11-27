7db81bd7a7732110f52858265ad08af0ba8e4fb2
EC2 Default User <ec2-user@ip-172-31-32-158.eu-west-1.compute.internal>
2020-11-27 21:13:38 +0000

    added proper logs on newplugin

--- src/mccli.js
+++ src/mccli.js
@@ -2,42 +2,58 @@
 
 const pluginStarterRepo = "https://github.com/kissmybutton/motorcortex-plugin-starter";
 const Git = require("nodegit");
-const {program} = require("@caporal/core");
-const rimraf = require("rimraf"); 
+const { program } = require("@caporal/core");
+const rimraf = require("rimraf");
 const { exec } = require("child_process");
+const process = require('process');
+
+const ora = require('ora');
 
 const cloneOptions = {
-  fetchOpts: {
-    callbacks: {
-      certificateCheck: function() { return 0; }
+    fetchOpts: {
+        callbacks: {
+            certificateCheck: function () { return 0; }
+        }
     }
-  }
 }
 program
-  .command('newplugin')
-  .argument("<name>", "Name of your plugin")
-  .action(({ logger, args, options }) => {
-    Git.Clone(pluginStarterRepo, args.name, cloneOptions).then(function(repository) {
-      // remove git files
-      if(process.platform === "win32"){
-        rimraf(`.\\${args.name}\\.git`, ()=>{});
-        rimraf(`.\\${args.name}\\.github`, ()=>{});
-      } else {
-        rimraf(`./${args.name}/.git`, ()=>{});
-        rimraf(`./${args.name}/.github`, ()=>{});
-      }
-      exec(`npm --prefix ${args.name} install ${args.name}`, (error, stdout, stderr) => {
-          if (error) {
-              console.log(`error: ${error.message}`);
-              return;
-          }
-          if (stderr) {
-              console.log(`stderr: ${stderr}`);
-              return;
-          }
-          console.log(`stdout: New plugin folder ready, node modules are installed`);
-      });
-    });
-  })
+    // new plugin
+    .command('newplugin')
+    .argument("<name>", "Name of your plugin")
+    .action(({ logger, args, options }) => {
+        logger.info('Cloning starter repo');
+        Git.Clone(pluginStarterRepo, args.name, cloneOptions).then(function (repository) {
+            // remove git files
+            if (process.platform === "win32") {
+                rimraf(`.\\${args.name}\\.git`, () => { });
+                rimraf(`.\\${args.name}\\.github`, () => { });
+            } else {
+                rimraf(`./${args.name}/.git`, () => { });
+                rimraf(`./${args.name}/.github`, () => { });
+            }
+            const throbber = ora('Installing dependencies. This will take a while').start();
+            exec(`npm --prefix ${args.name} install ${args.name}`, (error, stdout, stderr) => {
+                if (error) {
+                    throbber.stop();
+                    logger.error(error.message);
+                    return;
+                }
+                if (stderr) {
+                    //   logger.warn(stderr);
+                }
+                throbber.stop();
+                logger.info(`Your new plugin workspace is ready on folder "${args.name}". Have fun!!!`);
+            });
+        });
+    })
+
+    // new clip
+    .command('newclip')
+    .argument("<name>", "Name of your project")
+    .option('--with <plugins>', "Select plugins")
+    .action(({ logger, args, options }) => {
+        logger.info("%s, %s!", options.with, args.name);
+    }
+  )
 
 program.run();
