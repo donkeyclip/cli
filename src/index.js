2c883b871fa914a7b4fc1980dc5c0ce71d0f0326
alexdor <alexandros@dorodouli.com>
2021-10-06 07:33:26 +0000

    Deploying to gh-pages from @ donkeyclip/cli@cd532f0125c65b33f52b79abea53a4342f3b05c1 🚀

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 const yargs = require("yargs");
 
-const {
-    PLUGIN_STARTER_REPO,
-    CLIP_STARTER_REPO,
-    USAGE,
-    CLONE_OPTIONS 
-} = require("./config");
-const utils = require('./utils.js')
+const utils = require("./utils.js");
 
-const options = yargs.usage(utils.USAGE).argv;
-const command = yargs.argv._[0]
+const command = yargs.argv._[0];
 const projectName = yargs.argv._[1];
 
-if(!utils.checkCommand(command)) return utils.showHelp()
-if(!utils.checkName(projectName)) return utils.showHelp()
+if (!utils.checkCommand(command)) return utils.showHelp();
+if (!utils.checkName(projectName)) return utils.showHelp();
 utils.cloneGitRepo(command, projectName);
