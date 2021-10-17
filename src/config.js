87f0296f5bdf236f0fcdb50efa10eced70c2db45
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2021-10-17 16:35:43 +0300

    fix: add isomorphic-git and remove nodegit

--- src/config.js
+++ src/config.js
@@ -5,29 +5,22 @@ Available commands:
  - newplugin
  - newclip`;
 
- const MESSAGES = {
-    clone:"Cloning starter repo",
-    install:'Installing dependencies. This will take a while',
-    finish:(projectName)=>`Your project workspace is ready on folder "${projectName}". Be creative and have fun!`,
-    folderExists: (projectName) => `Folder "${projectName}" already exists`,
-    unexpectedError: "Unexpected error!"
+const MESSAGES = {
+  clone: "Cloning starter repo",
+  install: "Installing dependencies. This will take a while",
+  finish: (projectName) =>
+    `Your project workspace is ready on folder "${projectName}". Be creative and have fun!`,
+  folderExists: (projectName) => `Folder "${projectName}" already exists`,
+  unexpectedError: "Unexpected error!",
 };
 
-const CLONE_OPTIONS = {
-    fetchOpts: {
-        callbacks: {
-            certificateCheck: function () { return 0; }
-        }
-    }
-}
 const STARTER_REPO = {
-    newplugin:"https://github.com/donkeyclip/motorcortex-plugin-starter",
-    newclip:"https://github.com/donkeyclip/motorcortex-clip-starter"
+  newplugin: "https://github.com/donkeyclip/motorcortex-plugin-starter",
+  newclip: "https://github.com/donkeyclip/motorcortex-clip-starter",
 };
 
 module.exports = {
-    USAGE, 
-    CLONE_OPTIONS,
-    STARTER_REPO,
-    MESSAGES
-}
+  USAGE,
+  STARTER_REPO,
+  MESSAGES,
+};
--- src/utils.js
+++ src/utils.js
@@ -1,71 +1,81 @@
-
-const Git = require("nodegit");
-const {USAGE,MESSAGES,STARTER_REPO,CLONE_OPTIONS} = require("./config");
-const fs = require("fs")
+const Git = require("isomorphic-git");
+const { USAGE, MESSAGES, STARTER_REPO } = require("./config");
+const fs = require("fs");
 const rimraf = require("rimraf");
-const process = require('process');
+const process = require("process");
 const { exec } = require("child_process");
-const ora = require('ora');
-const {v4: uuidv4} = require("uuid")
+const ora = require("ora");
+const { v4: uuidv4 } = require("uuid");
+const http = require("isomorphic-git/http/node");
+const path = require("path");
 
 function checkCommand(command) {
-    return ["newplugin","newclip"].includes(command)
+  return ["newplugin", "newclip"].includes(command);
 }
 
 function checkName(name) {
-    return !!name
+  return !!name;
 }
 
-function showHelp(){
-    console.log(USAGE)
+function showHelp() {
+  console.log(USAGE);
 }
 
 function cloneGitRepo(command, projectName) {
-    console.log(MESSAGES.clone);
-    try {
-        if (fs.existsSync(projectName)) {
-          console.log(MESSAGES.folderExists(projectName))
+  console.log(MESSAGES.clone);
+  try {
+    if (fs.existsSync(projectName)) {
+      console.log(MESSAGES.folderExists(projectName));
+      return;
+    }
+  } catch (e) {
+    console.log(MESSAGES.unexpectedError, e);
+  }
+  const dir = path.join(process.cwd(), projectName);
+  Git.clone({
+    fs,
+    http,
+    dir,
+    url: STARTER_REPO[command],
+  })
+    .then(function () {
+      // remove git files
+      if (process.platform === "win32") {
+        rimraf(`.\\${projectName}\\.git`, () => {});
+        rimraf(`.\\${projectName}\\.github`, () => {});
+      } else {
+        rimraf(`./${projectName}/.git`, () => {});
+        rimraf(`./${projectName}/.github`, () => {});
+      }
+      const throbber = ora(MESSAGES.install).start();
+      exec(`npm install`, { cwd: projectName }, (error, stdout, stderr) => {
+        if (error) {
+          throbber.stop();
+          console.error(error.message);
           return;
         }
-      } catch(e) {
-        console.log(MESSAGES.unexpectedError,e)
-      }
-    Git.Clone(STARTER_REPO[command], projectName, CLONE_OPTIONS).then(function (repository) {
-        // remove git files
-        if (process.platform === "win32") {
-            rimraf(`.\\${projectName}\\.git`, () => { });
-            rimraf(`.\\${projectName}\\.github`, () => { });
-        } else {
-            rimraf(`./${projectName}/.git`, () => { });
-            rimraf(`./${projectName}/.github`, () => { });
+        if (stderr) {
+          //   console.warn(stderr);
         }
-        const throbber = ora(MESSAGES.install).start();
-        exec(`npm install`, {cwd: projectName}, (error, stdout, stderr) => {
-            if (error) {
-                throbber.stop();
-                console.error(error.message);
-                return;
-            }
-            if (stderr) {
-                //   console.warn(stderr);
-            }
-            
-            throbber.stop();
-            const idjsContent = `export default "${uuidv4()}";`
-            fs.writeFile(`${projectName}/clip/id.js`, idjsContent, err => {
-                if (err) {
-                  console.error(err)
-                  return
-                }
-                console.log(MESSAGES.finish(projectName));
-              })
+
+        throbber.stop();
+        const idjsContent = `export default "${uuidv4()}";`;
+        fs.writeFile(`${projectName}/clip/id.js`, idjsContent, (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log(MESSAGES.finish(projectName));
         });
-    });
+      });
+      return true;
+    })
+    .catch(console.error);
 }
 
 module.exports = {
-    checkCommand,
-    checkName,
-    showHelp,
-    cloneGitRepo
-}
+  checkCommand,
+  checkName,
+  showHelp,
+  cloneGitRepo,
+};
