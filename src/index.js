6c836d10e0e0754c3dcfb1ca050d760cc354f232
Theofilos Papadopoulos <priestont@gmail.com>
2023-01-21 16:09:03 +0200

    fix: windows compatibility isseus

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 #!/usr/bin/env node
 // #!/usr/bin/env node
-// const yargs = require("yargs");
 const utils = require("./utils.js");
-
-// const command = yargs.argv._[0];
-// const projectName = yargs.argv._[1];
-
-// if (!utils.checkCommand(command)) return utils.showHelp();
-// if (!utils.checkName(projectName)) return utils.showHelp();
-// utils.cloneGitRepo(command, projectName);
 
 const { Command } = require("commander");
 const program = new Command();
